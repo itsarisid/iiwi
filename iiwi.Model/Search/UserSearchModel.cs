1cf170cc9f3324097b745585f17bce0f65fd99fb
itsarisid <khanalex301@gmail.com>
2025-03-16 13:12:00 +0300

    Search model

---
+++ iiwi.Model/Search/UserSearchModel.cs
@@ -0,0 +1,11 @@
+﻿namespace iiwi.Model.Search;
+
+public class CustomerSearchModel : IDocument
+{
+    public string UniqueKey => TConst;
+
+    public required string TConst { get; set; }
+
+    [FacetProperty]
+    public required string Name { get; set; }
+}
