018d6784bc6ba4beed81972190748321e7e2857f
Sajid Khan <khanalex301@gmail.com>
2025-03-19 13:24:40 +0300

    User records

---
+++ iiwi.Model/Records/UserRecord.cs
@@ -0,0 +1,13 @@
+﻿
+namespace iiwi.Model.Records;
+
+public sealed record UserModel
+{
+    public long Id { get; init; }
+
+    public required string Name { get; init; }
+
+    public required string Email { get; init; }
+    public bool Status { get; set; }
+    public required string PhoneNumber { get; set; }
+}
