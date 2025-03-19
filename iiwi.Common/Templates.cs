b3edbc6dea058f5d064521bb53f26feda985f850
Sajid Khan <khanalex301@gmail.com>
2025-03-19 16:18:17 +0300

    Template

---
+++ iiwi.Common/Templates.cs
@@ -0,0 +1,10 @@
+﻿namespace iiwi.Common;
+
+public static class Templates
+{
+    public static string Register = "Register.html";
+    public static string ResetPassword = "ResetPassword.html";
+    public static string ConfirmEmail = "ConfirmEmail.html";
+    public static string ChangeEmail = "ChangeEmail.html";
+    public static string VerificationEmail = "VerificationEmail.html";
+}
