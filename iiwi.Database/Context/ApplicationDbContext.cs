97a163330c6d1e423cda6d436db9dcfa5e4f4f94
Sajid Khan <khanalex301@gmail.com>
2025-03-20 12:27:30 +0300

    DbContext

---
+++ iiwi.Database/Context/ApplicationDbContext.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+namespace iiwi.Database;
+
+public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<IdentityUser>(options)
+{
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.HasDefaultSchema("iiwi");
+
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+
+        builder.Entity<IdentityUser>(entity =>
+        {
+            entity.ToTable(name: "AppUser");
+        });
+
+        builder.Entity<IdentityRole>(entity =>
+        {
+            entity.ToTable(name: "Role");
+        });
+        builder.Entity<IdentityUserRole<string>>(entity =>
+        {
+            entity.ToTable("UserRoles");
+            //in case you changed the TKey type
+            //  entity.HasKey(key => new { key.UserId, key.RoleId });
+        });
+
+        builder.Entity<IdentityUserClaim<string>>(entity =>
+        {
+            entity.ToTable("UserClaims");
+        });
+
+        builder.Entity<IdentityUserLogin<string>>(entity =>
+        {
+            entity.ToTable("UserLogins");
+            //in case you changed the TKey type
+            //  entity.HasKey(key => new { key.ProviderKey, key.LoginProvider });       
+        });
+
+        builder.Entity<IdentityRoleClaim<string>>(entity =>
+        {
+            entity.ToTable("RoleClaims");
+
+        });
+
+        builder.Entity<IdentityUserToken<string>>(entity =>
+        {
+            entity.ToTable("UserTokens");
+            //in case you changed the TKey type
+            // entity.HasKey(key => new { key.UserId, key.LoginProvider, key.Name });
+
+        });
+    }
+}
+
---
+++ iiwi.Database/Context/DbContextFactory.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.Extensions.Configuration;
+
+namespace iiwi.Database;
+
+public class DbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        // Get environment
+        string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+        // Build config
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{environment}.json", optional: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        builder.UseSqlServer(connectionString, b => b.MigrationsAssembly("Architecture.Database"));
+
+        //builder.UseSqlServer(connectionString);
+
+        return new ApplicationDbContext(builder.Options);
+    }
+}
