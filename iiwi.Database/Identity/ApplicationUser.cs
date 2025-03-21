4d9855b29b36fd674a687880e0d11b7999cc18c0
Sajid Khan <khanalex301@gmail.com>
2025-03-21 20:26:55 +0300

    Property in identity

--- iiwi.Database/Identity/ApplicationUser.cs
+++ iiwi.Database/Identity/ApplicationUser.cs
@@ -4,8 +4,15 @@ namespace iiwi.Database.Identity;
 
 public class ApplicationUser : IdentityUser
 {
-    public string Address { get; set; }
+    [PersonalData]
     public string Gender { get; set; }
+    [PersonalData]
     public string FirstName { get; set; }
+    [PersonalData]
     public string LastName { get; set; }
+    public string DisplayName { get; set; }
+    [PersonalData]
+    public DateTime DOB { get; set; }
+    public string Address { get; set; }
+    public DateTime LastLogin { get; set; }
 }
