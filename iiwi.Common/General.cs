7a7dd4208b746cd9c90aab4e327a2c216b339625
Sajid Khan <khanalex301@gmail.com>
2025-03-22 16:46:13 +0300

    Test Controller

--- iiwi.Common/General.cs
+++ iiwi.Common/General.cs
@@ -3,5 +3,6 @@ namespace iiwi.Common;
 
 public static class General
 {
-    public static string SchemaName { get; set; } = "iiwi";
+    public static string SchemaName  = "iiwi";
+    public static string Files = "Files";
 }
