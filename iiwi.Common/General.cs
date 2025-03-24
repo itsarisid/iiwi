b89b32dde5ed36f2291f533318a7ea1b18002b8a
Sajid Khan <khanalex301@gmail.com>
2025-03-24 11:02:07 +0300

    General

--- iiwi.Common/General.cs
+++ iiwi.Common/General.cs
@@ -5,4 +5,5 @@ public static class General
 {
     public static string SchemaName  = "iiwi";
     public static string Files = "Files";
+    public static string AuthenticatorUriFormat = "otpauth://totp/{0}:{1}?secret={2}&issuer={0}&digits=6";
 }
