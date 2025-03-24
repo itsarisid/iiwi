a29aaecbf9d9f84c02c637d89ff6e697e3294892
Sajid Khan <khanalex301@gmail.com>
2025-03-24 10:11:26 +0300

    Password handler

--- iiwi.Application/Authentication/Password/SetPasswordHandler.cs
+++ iiwi.Application/Authentication/Password/SetPasswordHandler.cs
@@ -1,54 +1,59 @@
+using DotNetCore.Mediator;
+using DotNetCore.Results;
+using iiwi.Application.Provider;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
 
-namespace Architecture.Application.Authentication
+namespace iiwi.Application.Authentication;
+
+public class SetPasswordHandler(
+UserManager<IdentityUser> userManager,
+SignInManager<IdentityUser> signInManager,
+IClaimsProvider claimsProvider,
+ILogger<SetPasswordHandler> logger) : IHandler<SetPasswordRequest, Response>
 {
-    public class SetPasswordHandler(
-    UserManager<IdentityUser> userManager,
-    SignInManager<IdentityUser> signInManager,
-    IClaimsProvider claimsProvider,
-    ILogger<SetPasswordHandler> logger,
-    IResultService resultService) : IHandler<SetPasswordRequest, Response>
+    private readonly UserManager<IdentityUser> _userManager = userManager;
+    private readonly SignInManager<IdentityUser> _signInManager = signInManager;
+    private readonly IClaimsProvider _claimsProvider = claimsProvider;
+    private readonly ILogger<SetPasswordHandler> _logger = logger;
+
+    public async Task<Result<Response>> HandleAsync(SetPasswordRequest request)
     {
-        private readonly UserManager<IdentityUser> _userManager = userManager;
-        private readonly SignInManager<IdentityUser> _signInManager = signInManager;
-        private readonly IClaimsProvider _claimsProvider = claimsProvider;
-        private readonly IResultService _resultService = resultService;
-        private readonly ILogger<SetPasswordHandler> _logger = logger;
-
-        public async Task<Result<Response>> HandleAsync(SetPasswordRequest request)
+        var user = await _userManager.GetUserAsync(_claimsProvider.ClaimsPrinciple);
+        if (user == null)
         {
-            var user = await _userManager.GetUserAsync(_claimsProvider.ClaimsPrinciple);
-            if (user == null)
+            return new Result <Response>(HttpStatusCode.BadRequest,new Response
             {
-                return _resultService.Error<Response>($"Unable to load user with ID '{_userManager.GetUserId(_claimsProvider.ClaimsPrinciple)}'.");
-            }
-
-            var hasPassword = await _userManager.HasPasswordAsync(user);
-
-            if (hasPassword)
-            {
-                //Note: goto change password
-                return _resultService.Error<Response>("Password already set");
-            }
-
-            var addPasswordResult = await _userManager.AddPasswordAsync(user, request.NewPassword);
-            if (!addPasswordResult.Succeeded)
-            {
-                _logger.LogError("Error while setting password");
-                return _resultService.Error<Response>("Error while setting password");
-            }
-
-            await _signInManager.RefreshSignInAsync(user);
-            return _resultService.Success(new Response
-            {
-                Message = "Your password has been set.",
+               Message= $"Unable to load user with ID '{_userManager.GetUserId(_claimsProvider.ClaimsPrinciple)}'."
             });
         }
+
+        var hasPassword = await _userManager.HasPasswordAsync(user);
+
+        if (hasPassword)
+        {
+            //Note: goto change password
+            return new Result<Response>(HttpStatusCode.BadRequest, new Response
+            {
+                Message = "Password already set"
+            });
+        }
+
+        var addPasswordResult = await _userManager.AddPasswordAsync(user, request.NewPassword);
+        if (!addPasswordResult.Succeeded)
+        {
+            _logger.LogError("Error while setting password");
+            return new Result<Response>(HttpStatusCode.InternalServerError, new Response
+            {
+                Message = "Error while setting password"
+            });
+        }
+
+        await _signInManager.RefreshSignInAsync(user);
+        return new Result<Response>(HttpStatusCode.OK,new Response
+        {
+            Message = "Your password has been set.",
+        });
     }
 }
--- iiwi.Application/Authentication/Personal/DeletePersonalDataHandler.cs
+++ iiwi.Application/Authentication/Personal/DeletePersonalDataHandler.cs
@@ -5,57 +5,55 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Logging;
 using System.Net;
 
-namespace iiwi.Application.Authentication
+namespace iiwi.Application.Authentication;
+public class DeletePersonalDataHandler(
+UserManager<IdentityUser> userManager,
+SignInManager<IdentityUser> signInManager,
+IClaimsProvider claimsProvider,
+ILogger<LoginHandler> logger) : IHandler<DeletePersonalDataRequest, Response>
 {
-    public class DeletePersonalDataHandler(
-    UserManager<IdentityUser> userManager,
-    SignInManager<IdentityUser> signInManager,
-    IClaimsProvider claimsProvider,
-    ILogger<LoginHandler> logger) : IHandler<DeletePersonalDataRequest, Response>
-    {
-        private readonly UserManager<IdentityUser> _userManager = userManager;
-        private readonly SignInManager<IdentityUser> _signInManager = signInManager;
-        private readonly IClaimsProvider _claimsProvider = claimsProvider;
-        private readonly ILogger<LoginHandler> _logger = logger;
+    private readonly UserManager<IdentityUser> _userManager = userManager;
+    private readonly SignInManager<IdentityUser> _signInManager = signInManager;
+    private readonly IClaimsProvider _claimsProvider = claimsProvider;
+    private readonly ILogger<LoginHandler> _logger = logger;
 
-        public async Task<Result<Response>> HandleAsync(DeletePersonalDataRequest request)
+    public async Task<Result<Response>> HandleAsync(DeletePersonalDataRequest request)
+    {
+        var user = await _userManager.GetUserAsync(_claimsProvider.ClaimsPrinciple);
+        if (user == null)
         {
-            var user = await _userManager.GetUserAsync(_claimsProvider.ClaimsPrinciple);
-            if (user == null)
+            return new Result<Response>(HttpStatusCode.BadRequest, new Response
+            {
+                Message = $"Unable to load user with ID '{_userManager.GetUserId(_claimsProvider.ClaimsPrinciple)}'."
+            });
+        }
+
+        var RequirePassword = await _userManager.HasPasswordAsync(user);
+        if (RequirePassword)
+        {
+            if (!await _userManager.CheckPasswordAsync(user, request.Password))
             {
                 return new Result<Response>(HttpStatusCode.BadRequest, new Response
                 {
-                    Message = $"Unable to load user with ID '{_userManager.GetUserId(_claimsProvider.ClaimsPrinciple)}'."
+                    Message = "Incorrect password."
                 });
             }
-
-            var RequirePassword = await _userManager.HasPasswordAsync(user);
-            if (RequirePassword)
-            {
-                if (!await _userManager.CheckPasswordAsync(user, request.Password))
-                {
-                    return new Result<Response>(HttpStatusCode.BadRequest, new Response
-                    {
-                        Message = "Incorrect password."
-                    });
-                }
-            }
-
-            var result = await _userManager.DeleteAsync(user);
-            var userId = await _userManager.GetUserIdAsync(user);
-            if (!result.Succeeded)
-            {
-                throw new InvalidOperationException($"Unexpected error occurred deleting user with ID '{userId}'.");
-            }
-
-            await _signInManager.SignOutAsync();
-
-            _logger.LogInformation("User with ID '{UserId}' deleted themselves.", userId);
-            return new Result<Response>(HttpStatusCode.OK, new Response
-            {
-                Message = "Your record has been deleted",
-            });
         }
+
+        var result = await _userManager.DeleteAsync(user);
+        var userId = await _userManager.GetUserIdAsync(user);
+        if (!result.Succeeded)
+        {
+            throw new InvalidOperationException($"Unexpected error occurred deleting user with ID '{userId}'.");
+        }
+
+        await _signInManager.SignOutAsync();
+
+        _logger.LogInformation("User with ID '{UserId}' deleted themselves.", userId);
+        return new Result<Response>(HttpStatusCode.OK, new Response
+        {
+            Message = "Your record has been deleted",
+        });
     }
 }
 
