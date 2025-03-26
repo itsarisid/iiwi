66ae013d438c2b80d3fbaad9b95c837e299b405f
Sajid Khan <khanalex301@gmail.com>
2025-03-26 10:38:23 +0300

    Serilog Congfiguraton

--- iiwi.Database/Logs/SerilogConfiguration.cs
+++ iiwi.Database/Logs/SerilogConfiguration.cs
@@ -1,6 +1,4 @@
-﻿using iiwi.Common;
-using iiwi.Domain;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.EntityFrameworkCore;
 using iiwi.Domain.Logs;
 
@@ -12,11 +10,11 @@ public sealed class SerilogConfiguration : IEntityTypeConfiguration<Serilog>
     {
         builder.ToTable(nameof(Serilog), "Log");
         builder.HasKey(entity => entity.Id);
-        builder.Property(entity => entity.Message).HasMaxLength(-1;
-        builder.Property(entity => entity.MessageTemplate).HasMaxLength(-1;
-        builder.Property(entity => entity.Level).HasMaxLength(-1;
+        builder.Property(entity => entity.Message).HasMaxLength(-1);
+        builder.Property(entity => entity.MessageTemplate).HasMaxLength(-1);
+        builder.Property(entity => entity.Level).HasMaxLength(-1);
         builder.Property(entity => entity.TimeStamp);
-        builder.Property(entity => entity.Exception).HasMaxLength(-1;
+        builder.Property(entity => entity.Exception).HasMaxLength(-1);
         builder.Property(entity => entity.Properties).HasMaxLength(-1);
     }
 }
