c5823e2d6af5f0eb74872378540de37fa70ed262
Sajid Khan <khanalex301@gmail.com>
2025-03-26 13:45:03 +0300

    some extra policy

--- iiwi.AppWire/Program.cs
+++ iiwi.AppWire/Program.cs
@@ -9,12 +9,15 @@ using iiwi.Database;
 using iiwi.Model.Settings;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using Serilog;
 using Serilog.Ui.Core.Extensions;
 using Serilog.Ui.MsSqlServerProvider.Extensions;
 using Serilog.Ui.Web.Extensions;
+using Serilog.Ui.Web.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -43,23 +46,47 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlSer
 builder.Services.AddIdentity();
 
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme,
-        options => builder.Configuration.Bind("JwtSettings", options))
+builder.Services.AddAuthentication()
+    //.AddJwtBearer(JwtBearerDefaults.AuthenticationScheme,
+    //    options => builder.Configuration.Bind("JwtSettings", options))
+    
+    //.AddCookie(CookieAuthenticationDefaults.AuthenticationScheme,
+    //    options => builder.Configuration.Bind("CookieSettings", options));
+    //.AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, config =>
+    //{
+    //    config.Cookie.Name = "iiwi.Cookie";
+    //    config.LoginPath = new PathString("/auth/login");
+    //    config.AccessDeniedPath = new PathString("/auth/denied");
+    //    config.ExpireTimeSpan = TimeSpan.FromDays(7);
+    //    config.SlidingExpiration = true;
+    //    builder.Configuration.Bind("CookieSettings", config);
+    //})
     .AddJwtBearer(options =>
     {
         options.Authority = "http://localhost:5093";
         options.Audience = "iiwi";
-        options.TokenValidationParameters = new TokenValidationParameters()
+        options.ClaimsIssuer = "Harmony";
+        //options.IncludeErrorDetails = true;
+        options.TokenValidationParameters = new TokenValidationParameters
         {
-            NameClaimType = "name"
+            NameClaimType = "Subject",
+            RoleClaimType = "Role"
         };
-    })
-    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme,
-        options => builder.Configuration.Bind("CookieSettings", options));
+    });
 
-builder.Services.AddAuthorization();
 builder.Services.AddAuthorizationBuilder().AddPolicy("TwoFactorEnabled", x => x.RequireClaim("amr", "mfa"));
+//builder.Services.AddAuthorization();
+
+builder.Services.AddAuthorization(options =>
+{
+    var defaultAuthorizationPolicyBuilder = new AuthorizationPolicyBuilder(
+        JwtBearerDefaults.AuthenticationScheme);
+    defaultAuthorizationPolicyBuilder =
+        defaultAuthorizationPolicyBuilder.RequireAuthenticatedUser();
+    options.DefaultPolicy = defaultAuthorizationPolicyBuilder.Build();
+});
+
+//builder.Services.AddAuthentication().AddIdentityServerJwt();
 
 builder.Services.AddClassesMatchingInterfaces(nameof(iiwi));
 builder.Services.AddMediator(nameof(iiwi));
