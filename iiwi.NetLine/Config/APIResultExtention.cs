8b6236830b922906076ac78fce1fcc14e61a66aa
Sajid Khan <khanalex301@gmail.com>
2025-04-13 00:16:39 +0300

    Convert api result into http response

---
+++ iiwi.NetLine/Config/APIResultExtention.cs
@@ -0,0 +1,22 @@
+﻿using DotNetCore.Results;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iiwi.NetLine.Config;
+
+public static class APIResultExtention
+{
+    public static HttpResponse ApiResult<T>(this Result<T> result)
+    {
+        //return new ObjectResult(result.HasMessage ? result.Message : ((object)result.Value))
+        //{
+        //    StatusCode = (int)result.Status
+        //};
+
+        return HttpResponse
+        {
+            StatusCode = (int)result.Status,
+            ContentType = "application/json",
+            Body = result.HasMessage ? result.Message : ((object)result.Value)
+        };
+    }
+}
--- iiwi.NetLine/Modules/HomeModule.cs
+++ iiwi.NetLine/Modules/HomeModule.cs
@@ -2,19 +2,27 @@
 using DotNetCore.Mediator;
 using DotNetCore.Results;
 using iiwi.Application;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace iiwi.NetLine.Modules;
 
 public class HomeModule : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
-    {   app.MapPost("/home",  Task<Result<Response>>
+    {   app.MapPost("/home", async Task<Result<Response>>
         (IMediator mediator, UpdateProfileRequest request, Result<Response> response) => 
         {
-            var res =   mediator.HandleAsync<UpdateProfileRequest, Response>(request).ApiResult();
+            var res =   await mediator.HandleAsync<UpdateProfileRequest, Response>(request);
 
+            return TypedResults.Ok(new Response
+            {
+                Message=res.Message,
 
-            return TypedResults.Ok<Response>(res);
+            });
+
+            //return TypedResults.Ok<Response>(res);
         })
         .WithTags("Home")
         .WithName("Index")
