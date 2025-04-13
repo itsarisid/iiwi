9f5af7be2629ba08199d2956ca3afa82be39890c
Sajid Khan <khanalex301@gmail.com>
2025-04-13 12:30:33 +0300

    Extension methods

--- iiwi.NetLine/Config/APIResultExtention.cs
+++ iiwi.NetLine/Config/APIResultExtention.cs
@@ -1,22 +1,65 @@
 ﻿using DotNetCore.Results;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Reflection.Metadata;
 
 namespace iiwi.NetLine.Config;
 
 public static class APIResultExtention
 {
-    public static HttpResponse ApiResult<T>(this Result<T> result)
+    public static IResult MyResult<T>(this Result<T> result)
     {
-        //return new ObjectResult(result.HasMessage ? result.Message : ((object)result.Value))
+        if (result == null)
+        {
+            return TypedResults.NotFound();
+        }
+        if (result.Status == HttpStatusCode.OK)
+        {
+            return TypedResults.Ok(result.Value);
+        }
+        return TypedResults.Empty;
+    }
+
+
+    public static async Task<TResult> Convert<TSource, TResult>(
+    this Task<TSource> task, Func<TSource, TResult> projection)
+    {
+        var result = await task.ConfigureAwait(false);
+        return projection(result);
+    }
+
+    public static IResult MyResult<T>(this Task<Result<T>> result)
+    {
+        return result.Result.MyResult();
+    }
+
+    public static IResult MyResult(this Result result)
+    {
+        //return new ObjectResult(result.Message)
         //{
         //    StatusCode = (int)result.Status
         //};
-
-        return HttpResponse
-        {
-            StatusCode = (int)result.Status,
-            ContentType = "application/json",
-            Body = result.HasMessage ? result.Message : ((object)result.Value)
-        };
+        return TypedResults.Ok(result);
     }
+
+    public static IResult MyResult(this Task<Result> result)
+    {
+        return result.Result.MyResult();
+    }
+
+    public static IResult MyResult<T>(this T result)
+    {
+        return TypedResults.Ok(result);
+    }
+
+    public static IResult MyResult<T>(this Task<T> result)
+    {
+        return result.Result.MyResult();
+    }
+
+    private static ValidationProblem CreateValidationProblem(string errorCode, string errorDescription) =>
+        TypedResults.ValidationProblem(new Dictionary<string, string[]> {
+            { errorCode, [errorDescription] }
+        });
 }
--- iiwi.NetLine/Modules/HomeModule.cs
+++ iiwi.NetLine/Modules/HomeModule.cs
@@ -1,29 +1,18 @@
 ﻿using Carter.OpenApi;
 using DotNetCore.Mediator;
-using DotNetCore.Results;
 using iiwi.Application;
-using Microsoft.AspNetCore.Identity.Data;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using iiwi.Application.Account;
+using iiwi.NetLine.Config;
 
 namespace iiwi.NetLine.Modules;
 
 public class HomeModule : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
-    {   app.MapPost("/home", async Task<Result<Response>>
-        (IMediator mediator, UpdateProfileRequest request, Result<Response> response) => 
-        {
-            var res =   await mediator.HandleAsync<UpdateProfileRequest, Response>(request);
-
-            return TypedResults.Ok(new Response
-            {
-                Message=res.Message,
-
-            });
-
-            //return TypedResults.Ok<Response>(res);
-        })
+    {
+        app.MapPost("/home", (IMediator mediator, UpdateProfileRequest request, Response response) =>mediator
+        .HandleAsync<UpdateProfileRequest, Response>(request)
+        .MyResult())
         .WithTags("Home")
         .WithName("Index")
         .RequireAuthorization()
