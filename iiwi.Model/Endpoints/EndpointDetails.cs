c05deb69fe26d23f53fc76c4d4ea5092aaae6199
Sajid Khan <khanalex301@gmail.com>
2025-04-25 16:44:45 +0300

    Endpoints details

---
+++ iiwi.Model/Endpoints/EndpointDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace iiwi.Model;
+
+public class EndpointDetails
+{
+    public string Endpoint { get; set; }
+    public string Name { get; set; }
+    public string Summary { get; set; }
+    public string Description { get; set; }
+    public string Tags { get; set; }
+}
