aa4952e62960586fab38d302d9b9563d7bc56df4
Sajid Khan <khanalex301@gmail.com>
2025-07-21 10:21:47 +0300

    Removed code

--- iiwi.Common/General.cs
+++ iiwi.Common/General.cs
@@ -6,8 +6,6 @@ public static class General
     public const string SchemaName = "iiwi";
     public const string DbSequenceName = "iiwi.Sequence";
     public const string DbConnectionName = "DefaultConnection";
-    //public const string IdentitySchemaName = "Identity";
-    //public const string LogSchemaName = "Log";
     public const string Files = "Files";
     public const string AuthenticatorUriFormat = "otpauth://totp/{0}:{1}?secret={2}&issuer={0}&digits=6";
 
