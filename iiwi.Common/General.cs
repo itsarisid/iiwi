3e04dedec5cbe1571c0db3e6c5d7e312222f7ede
Sajid Khan <khanalex301@gmail.com>
2025-07-21 10:22:21 +0300

    Removed file

--- iiwi.Common/General.cs
+++ iiwi.Common/General.cs
@@ -6,7 +6,6 @@ public static class General
     public const string SchemaName = "iiwi";
     public const string DbSequenceName = "iiwi.Sequence";
     public const string DbConnectionName = "DefaultConnection";
-    public const string Files = "Files";
     public const string AuthenticatorUriFormat = "otpauth://totp/{0}:{1}?secret={2}&issuer={0}&digits=6";
 
     public static class Schema
