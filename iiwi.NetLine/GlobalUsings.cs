33b04c2b93dc86324c08d2b58a64809465990286
Sajid Khan <khanalex301@gmail.com>
2025-11-03 20:23:09 +0300

    Add XML documentation to members and remove minor unused usings

--- iiwi.NetLine/GlobalUsings.cs
+++ iiwi.NetLine/GlobalUsings.cs
@@ -1,7 +1,7 @@
-﻿global using Carter;
+global using Carter;
 global using Carter.OpenApi;
 global using DotNetCore.Mediator;
 global using iiwi.NetLine.Config;
-global using  iiwi.Application;
+global using iiwi.Application;
 global using iiwi.NetLine.Extentions;
 global using iiwi.NetLine.Endpoints;
